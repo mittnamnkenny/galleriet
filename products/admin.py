adeea1ae5a5631a9f59029d1dbc574ca5f1d32a2
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-10-03 09:52:13 +0000

    Customize products admin and test add products

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import Product, Category, Artist
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'artist',
+        'category',
+        'medium',
+        'price',
+        'added_on',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ArtistAdmin(admin.ModelAdmin):
+    list_display = (
+        'full_name',
+        'nationality',
+        'email',
+        'phone_number',
+        'added_on',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Artist, ArtistAdmin)
