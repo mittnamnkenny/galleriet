7d284aa5f1b2c8bfb64148826dca105055db4f03
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-10-05 10:53:23 +0000

    Test add to bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
+
+
+def add_to_bag(request, item_id):
+    """ Add a product to the shopping bag """
+
+    redirect_url = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    bag[item_id] = 1
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirect_url)
