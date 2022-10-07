1c3a36f9c7a4b8c187edd32774ac51830693eba8
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-10-07 09:21:46 +0000

    Add preview for bootstrap toast success message

--- bag/views.py
+++ bag/views.py
@@ -37,4 +37,5 @@ def remove_from_bag(request, item_id):
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
