31a3937435c4b2b11e3d5b60d6305642c9422a7d
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-10-17 10:32:18 +0000

    Customize contact form style and placeholder text

--- contact/forms.py
+++ contact/forms.py
@@ -6,3 +6,27 @@ class ContactForm(ModelForm):
     class Meta:
         model = Contact
         fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full name',
+            'email': 'Email',
+            'message': 'Your message',
+        }
+
+        self.fields['subject'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'subject':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = \
+                'border-grey contact-form-input'
+            self.fields[field].label = False
