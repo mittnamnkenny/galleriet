ae2dda618c8440700a2334de7d769f7162fbdd84
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-10-17 09:33:54 +0000

    Update contact view, add form to template and test contact functionality

--- contact/views.py
+++ contact/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.contrib import messages
 
 
 def contact(request):
-    return render(request, 'contact/contact.html')
+    """ A view to return the contact page and form """
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Message successfully sent.')
+        else:
+            messages.error(
+                request,
+                'Failed to send message. Please ensure the form is valid.')
+    form = ContactForm()
+    context = {'form': form}
+    return render(request, 'contact/contact.html', context)
