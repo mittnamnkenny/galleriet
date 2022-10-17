eee46da655f29ee8985664c46fcfc25408786a69
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-10-17 08:34:31 +0000

    Create contact forms.py and add contact admin

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Contact
 
-# Register your models here.
+
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'subject',
+        'full_name',
+        'email',
+        'message'
+    )
+
+
+admin.site.register(Contact, ContactAdmin)
