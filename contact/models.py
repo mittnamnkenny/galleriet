fdc51564ebd7f7e3af8d2ab26cdc6bb594f9d0ed
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-10-17 08:20:35 +0000

    Add contact model and migrate

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+SUBJECT = (
+    (0, 'Customer service'),
+    (1, 'Technical assistance')
+)
+
+
+class Contact(models.Model):
+    """
+    Database model for Contact
+    """
+    subject = models.IntegerField(choices=SUBJECT, default=0)
+    full_name = models.CharField(max_length=254)
+    email = models.EmailField(max_length=254)
+    message = models.TextField()
+
+    def __str__(self):
+        return self.full_name
