ed9da6b4812387355f04c717aed20b17cf7d889c
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-10-19 10:58:10 +0000

    Update index view to return recent products

--- home/views.py
+++ home/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from products.models import Product
 
 
 def index(request):
-    """ A view to return the index page """
+    """ A view to return the index page and recent products """
+    products = Product.objects.all().order_by('-added_on')[:3]
 
-    return render(request, 'home/index.html')
+    context = {'products': products}
+
+    return render(request, 'home/index.html', context)
