0888b4d5c8449b8d957bd616a9faf14a1bbb7993
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-10-23 10:42:57 +0000

    Remove secret key and set debug

--- galleriet/settings.py
+++ galleriet/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%w&u^#g1f4bk^5c+9=m)9#gs+90d)wa4h98wspnx6vzz-+$3$k'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['galleriet-mittnamnkenny.herokuapp.com', 'localhost']
 
