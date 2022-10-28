bbc13b750f70a65d45fc0d0d806ff150cf69625c
mittnamnkenny <kenny.appelqvist@gmail.com>
2022-10-28 09:20:20 +0000

    Create privacy policy page and link to access it in footer

--- home/views.py
+++ home/views.py
@@ -9,3 +9,9 @@ def index(request):
     context = {'products': products}
 
     return render(request, 'home/index.html', context)
+
+
+def privacy_policy(request):
+    """ A view to return the privacy policy page """
+
+    return render(request, 'home/privacy_policy.html')
